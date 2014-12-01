7ebf89f3d3bb92851db6e18f6d9ec74ccd0c1641
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2014-12-01 08:45:28 -0700

    Get recursive translation working!
    This works.

--- i18n-translate.py
+++ i18n-translate.py
@@ -26,11 +26,13 @@ def translate_item(item_to_translate, translate_lang, goslate_instance):
             translated_data[i] = translate_item(item_to_translate, translate_lang, goslate_instance)
     return translated_data
 
-def translate_recursive(data_to_translate, translate_lang, gostate_instance):
+def translate_recursive(data_to_translate, translate_lang, goslate_instance):   # This works!
     translated_data = data_to_translate
-    for i in item_to_translate:
+    for i in data_to_translate:
+        print i
+        print translated_data[i]
         if isinstance(data_to_translate[i], dict):
-            translate_recursive(data_to_translate, translate_lang, gostate_instance)
+            translated_data[i] = translate_recursive(data_to_translate[i], translate_lang, goslate_instance)
         else:
-            translated_data = gostate_instance(data_to_translate[i[0], translate_lang])
-    return tranlated_data
+            translated_data[i] = goslate_instance.translate(data_to_translate[i], translate_lang)
+    return translated_data
