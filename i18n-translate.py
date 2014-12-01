33eb54e854fd9348bf13acfe87b2debf8b01752a
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2014-12-01 08:54:27 -0700

    Remove nonfunctional (broken) functions

--- i18n-translate.py
+++ i18n-translate.py
@@ -27,24 +27,3 @@ print original_data
 
 print "The translated data is:"
 print translate_recursive(original_data, sys.argv[2], gs)
-
-
-# Does not work
-def translate_dict(data_to_translate, translate_lang, goslate_instance):
-    for i in data_to_translate:
-        print i
-        if isinstance(data_to_translate[i], dict):
-            data_to_translate[i] = translate_item(
-                data_to_translate[i], translate_lang, goslate_instance)
-        else:
-            print goslate_instance.translate(data_to_translate[i], translate_lang)
-
-
-# Does not work
-def translate_item(item_to_translate, translate_lang, goslate_instance):
-    translated_data = {}
-    if isinstance(item_to_translate, dict) or True:
-        for i in item_to_translate:
-            translated_data[i] = translate_item(
-                item_to_translate, translate_lang, goslate_instance)
-    return translated_data
